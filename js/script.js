8f1f66f7758a50e5a65d80e23d1f1253a0a53fba
mikebeaujean <mbeaujean88@gmail.com>
2014-05-19 18:41:57 -0400

    fixed some js indentation

--- js/script.js
+++ js/script.js
@@ -19,24 +19,24 @@ function include(filename, onload) {
 
 include('http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js', function() {
 
-    $(document).ready(function() {
-      $("#header").hide(0).delay(100).fadeIn(1000);
-      $("#dashboard").hide(0).delay(1000).fadeIn(1000);
-      $("#welcome").hide(0).delay(2000).fadeIn(1000);
-      $("#contact-me").hide(0).delay(3000).fadeIn(1000);
+$(document).ready(function() {
+  $("#header").hide(0).delay(100).fadeIn(1000);
+  $("#dashboard").hide(0).delay(1000).fadeIn(1000);
+  $("#welcome").hide(0).delay(2000).fadeIn(1000);
+  $("#contact-me").hide(0).delay(3000).fadeIn(1000);
 
-      $("#about-me").hide();
-      $("#portfolio").hide();
+  $("#about-me").hide();
+  $("#portfolio").hide();
 
-      $("#about-me-link").on("click", function(){
-        $("#portfolio").fadeOut(500);
-        $("#about-me").hide(0).delay(500).fadeIn(1000);
-      });
+  $("#about-me-link").on("click", function(){
+    $("#portfolio").fadeOut(500);
+    $("#about-me").hide(0).delay(500).fadeIn(1000);
+  });
 
-      $("#portfolio-link").on("click", function(){
-        $("#about-me").fadeOut(500);
-        $("#portfolio").hide(0).delay(500).fadeIn(1000);
-      });
+  $("#portfolio-link").on("click", function(){
+    $("#about-me").fadeOut(500);
+    $("#portfolio").hide(0).delay(500).fadeIn(1000);
+  });
 
-    });
+  });
 });
