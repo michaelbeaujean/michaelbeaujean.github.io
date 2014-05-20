319fb450fa5230ae7d8423177ad668afe1c97047
mikebeaujean <mbeaujean88@gmail.com>
2014-05-20 16:23:20 -0400

    external jquery file

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
-function include(filename, onload) {
-    var head = document.getElementsByTagName('head')[0];
-    var script = document.createElement('script');
-    script.src = filename;
-    script.type = 'text/javascript';
-    script.onload = script.onreadystatechange = function() {
-        if (script.readyState) {
-            if (script.readyState === 'complete' || script.readyState === 'loaded') {
-                script.onreadystatechange = null;
-                onload();
-            }
-        }
-        else {
-            onload();
-        }
-    };
-    head.appendChild(script);
-}
+// function include(filename, onload) {
+//     var head = document.getElementsByTagName('head')[0];
+//     var script = document.createElement('script');
+//     script.src = filename;
+//     script.type = 'text/javascript';
+//     script.onload = script.onreadystatechange = function() {
+//         if (script.readyState) {
+//             if (script.readyState === 'complete' || script.readyState === 'loaded') {
+//                 script.onreadystatechange = null;
+//                 onload();
+//             }
+//         }
+//         else {
+//             onload();
+//         }
+//     };
+//     head.appendChild(script);
+// }
 
-include('http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js', function() {
+// include('http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js', function() {
 
 $(document).ready(function() {
   $("#header").hide(0).delay(100).fadeIn(1000);
@@ -41,4 +41,4 @@ $(document).ready(function() {
   });
 
   });
-});
+// });
